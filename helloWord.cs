d36f0a05799816ee58b14f4ace5f2742fbd610df
LeandroCina <165568347+LeandroCina@users.noreply.github.com>
2024-03-31 15:55:45 +0200

    Update helloWord.cs

--- helloWord.cs
+++ helloWord.cs
@@ -7,7 +7,9 @@ public class helloWord : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+    print("Hello !!!");
+    print(2024);
+
     }
 
     // Update is called once per frame
